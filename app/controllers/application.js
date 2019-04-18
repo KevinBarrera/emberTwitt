d751984c073dfc92039ccfed628aa2f61e53dfb4
KevinBarrera <kev97barrera@gmail.com>
2019-04-17 20:56:17 -0500

    GET y POST ember

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -11,13 +11,22 @@ export default class ApplicationController extends Controller {
 
 	@action
 	onSubmit (text){
-		let tweetsCopy = this.tweets.slice();
+		let post = this.store.createRecord('draft_tweet', {
+			avatar: "https://image.flaticon.com/icons/png/512/10/10906.png",
+			user_name: "KevinBH",
+			description: text,
+			created_at: "2019-04-17"
+		});
+
+		post.save(); // => POST to '/posts'
+
+		/* let tweetsCopy = this.tweets.slice();
 
 		this.tweets = tweetsCopy.concat({
 			id: Math.floor(Math.random() * 100) + 3,
 			user_name: "KevinBH",
 			description: text,
 			created_at: "2019-04-11"
-		});
+		}); */
 	}
 }
